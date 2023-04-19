a4784fdaa03189ee7eee601571731a2eb345bb4b
Kim <kroch@stud.hs-bremen.de>
2023-04-19 13:25:08 +0200

    x

--- game.js
+++ game.js
@@ -2,6 +2,13 @@ var config = {
   type: Phaser.AUTO,
   width: 800,
   height: 600,
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 00 },
+    },
+  },
+
   scene: {
     preload: preload,
     create: create,
@@ -13,13 +20,31 @@ var game = new Phaser.Game(config);
 
 function preload() {
   this.load.setBaseURL("http://labs.phaser.io");
-  this.load.image("bg", "assets/sets/background.png");
-  this.load.image("plattform3", "assets/sets/objects/platform2.png");
+  this.load.image("sky", "src/games/firstgame/assets/sky.png");
+  this.load.image("block", "assets/sprites/block.png");
+  this.load.image("block2", "assets/sprites/block.png");
 }
-var platforms;
+var block;
+var cursors;
+var block2;
 function create() {
-  this.add.image(400, 300, "bg");
-  this.add.image(400, 300, "plattform3");
+  this.add.image(400, 300, "sky");
+  block = this.physics.add.image(100, 100, "block");
+  block2 = this.physics.add.image(500, 500, "block2");
+  cursors = this.input.keyboard.createCursorKeys();
 }
 
-function update() {}
+function update() {
+  if (cursors.left.isDown) {
+    block.setVelocityX(-160);
+  } else if (cursors.right.isDown) {
+    block.setVelocityX(160);
+  } else if (cursors.up.isDown) {
+    block.setVelocityY(-160);
+  } else if (cursors.down.isDown) {
+    block.setVelocityY(160);
+  } else {
+    block.setVelocityX(0);
+    block.setVelocityY(0);
+  }
+}
